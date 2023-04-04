d920c14f877f5e512afa96cf9991a39f2a9b9ced
Lucio Gastellu Arrieta <104010048+lucio1907@users.noreply.github.com>
2023-04-04 15:50:45 -0300

    Desafio-15

---
+++ Desafio-15/src/controllers/routesControllers.controller.js
@@ -0,0 +1,32 @@
+import info from "../utils/info.js";
+import { logConfiguration, randomNumber } from "../utils/utils.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const logger = logConfiguration.getLogger(process.env.NODE_ENV);
+
+const getInfo = (req, res) => {
+  logger.info(`Route: ${req.url}, Method: ${req.method}`);
+  res.json(info);
+};
+
+const getRandomNumbers = (req, res) => {
+    logger.info(`Route: ${req.url}, Method: ${req.method}`);
+
+    if (!req.query.quantity) {
+        logger.error(`Route: ${req.url}, Method: ${req.method}, ErrorMessage: quantity not specified`);
+        res.status(400).json({ message: "You must to specify the quantity of numbers to generate" });
+    } else {
+        const quantity = req.query.quantity;
+        const random = randomNumber(quantity);
+        res.json(random);
+    }
+}
+
+const routeNotSpecified = (req, res) => {
+    logger.warn(`Route: ${req.url}, Method: ${req.method}, ErrorMessage: nonexistent route`);
+    res.status(404).json({ message: "Route not found" });
+}
+
+export { getInfo, getRandomNumbers, routeNotSpecified };
